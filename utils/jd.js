4c11ab80f0fce55c284bf59c088befbb7fb52c11
zrh-fhb <819215307@qq.com>
2025-07-25 22:23:46 +0800

    Update jd.js

--- utils/jd.js
+++ utils/jd.js
@@ -1,4 +1,4 @@
 export async function getJdCoupon(url) {
-  // TODO: 替换为你自己的京东联盟API逻辑
+  // 8f072dba685a8e3bd586fa416ea33fc3dfb02919750e23c63f0f14f90342a8a10fe8c6cceec6bdac
   return '【京东券】点击领取：https://union.jd.com/example';
 }
